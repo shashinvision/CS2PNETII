76e34b36a9bd2a3feb979ccfae556631e02aca48
Felipe Mancilla <felipei.mancilla@gmail.com>
2021-11-22 14:43:28 -0300

    Destinos

--- CS2PNETII/Form1.Designer.vb
+++ CS2PNETII/Form1.Designer.vb
@@ -22,10 +22,286 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.destino6y = New System.Windows.Forms.DomainUpDown()
+        Me.destino5y = New System.Windows.Forms.DomainUpDown()
+        Me.destino4y = New System.Windows.Forms.DomainUpDown()
+        Me.destino3y = New System.Windows.Forms.DomainUpDown()
+        Me.destino2y = New System.Windows.Forms.DomainUpDown()
+        Me.destino1y = New System.Windows.Forms.DomainUpDown()
+        Me.destino6x = New System.Windows.Forms.DomainUpDown()
+        Me.destino5x = New System.Windows.Forms.DomainUpDown()
+        Me.destino4x = New System.Windows.Forms.DomainUpDown()
+        Me.destino3x = New System.Windows.Forms.DomainUpDown()
+        Me.destino2x = New System.Windows.Forms.DomainUpDown()
+        Me.destino1x = New System.Windows.Forms.DomainUpDown()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.GroupBox1)
+        Me.Panel1.Location = New System.Drawing.Point(2, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(894, 562)
+        Me.Panel1.TabIndex = 0
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.LightGray
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.Label8)
+        Me.GroupBox1.Controls.Add(Me.Label7)
+        Me.GroupBox1.Controls.Add(Me.destino6y)
+        Me.GroupBox1.Controls.Add(Me.destino5y)
+        Me.GroupBox1.Controls.Add(Me.destino4y)
+        Me.GroupBox1.Controls.Add(Me.destino3y)
+        Me.GroupBox1.Controls.Add(Me.destino2y)
+        Me.GroupBox1.Controls.Add(Me.destino1y)
+        Me.GroupBox1.Controls.Add(Me.destino6x)
+        Me.GroupBox1.Controls.Add(Me.destino5x)
+        Me.GroupBox1.Controls.Add(Me.destino4x)
+        Me.GroupBox1.Controls.Add(Me.destino3x)
+        Me.GroupBox1.Controls.Add(Me.destino2x)
+        Me.GroupBox1.Controls.Add(Me.destino1x)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.GroupBox1.Location = New System.Drawing.Point(24, 30)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(247, 327)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Coordenadas"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(164, 19)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(14, 15)
+        Me.Label8.TabIndex = 19
+        Me.Label8.Text = "Y"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(81, 19)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(15, 15)
+        Me.Label7.TabIndex = 18
+        Me.Label7.Text = "X"
+        '
+        'destino6y
+        '
+        Me.destino6y.Enabled = False
+        Me.destino6y.Location = New System.Drawing.Point(164, 208)
+        Me.destino6y.Name = "destino6y"
+        Me.destino6y.Size = New System.Drawing.Size(77, 23)
+        Me.destino6y.TabIndex = 17
+        '
+        'destino5y
+        '
+        Me.destino5y.Enabled = False
+        Me.destino5y.Location = New System.Drawing.Point(164, 172)
+        Me.destino5y.Name = "destino5y"
+        Me.destino5y.Size = New System.Drawing.Size(77, 23)
+        Me.destino5y.TabIndex = 16
+        '
+        'destino4y
+        '
+        Me.destino4y.Enabled = False
+        Me.destino4y.Location = New System.Drawing.Point(164, 143)
+        Me.destino4y.Name = "destino4y"
+        Me.destino4y.Size = New System.Drawing.Size(77, 23)
+        Me.destino4y.TabIndex = 15
+        '
+        'destino3y
+        '
+        Me.destino3y.Enabled = False
+        Me.destino3y.Location = New System.Drawing.Point(164, 110)
+        Me.destino3y.Name = "destino3y"
+        Me.destino3y.Size = New System.Drawing.Size(77, 23)
+        Me.destino3y.TabIndex = 14
+        '
+        'destino2y
+        '
+        Me.destino2y.Enabled = False
+        Me.destino2y.Location = New System.Drawing.Point(164, 76)
+        Me.destino2y.Name = "destino2y"
+        Me.destino2y.Size = New System.Drawing.Size(77, 23)
+        Me.destino2y.TabIndex = 13
+        '
+        'destino1y
+        '
+        Me.destino1y.Enabled = False
+        Me.destino1y.Location = New System.Drawing.Point(164, 46)
+        Me.destino1y.Name = "destino1y"
+        Me.destino1y.Size = New System.Drawing.Size(77, 23)
+        Me.destino1y.TabIndex = 12
+        '
+        'destino6x
+        '
+        Me.destino6x.Enabled = False
+        Me.destino6x.Location = New System.Drawing.Point(81, 208)
+        Me.destino6x.Name = "destino6x"
+        Me.destino6x.Size = New System.Drawing.Size(77, 23)
+        Me.destino6x.TabIndex = 11
+        '
+        'destino5x
+        '
+        Me.destino5x.Enabled = False
+        Me.destino5x.Location = New System.Drawing.Point(81, 176)
+        Me.destino5x.Name = "destino5x"
+        Me.destino5x.Size = New System.Drawing.Size(77, 23)
+        Me.destino5x.TabIndex = 10
+        '
+        'destino4x
+        '
+        Me.destino4x.Enabled = False
+        Me.destino4x.Location = New System.Drawing.Point(81, 143)
+        Me.destino4x.Name = "destino4x"
+        Me.destino4x.Size = New System.Drawing.Size(77, 23)
+        Me.destino4x.TabIndex = 9
+        '
+        'destino3x
+        '
+        Me.destino3x.Enabled = False
+        Me.destino3x.Location = New System.Drawing.Point(81, 108)
+        Me.destino3x.Name = "destino3x"
+        Me.destino3x.Size = New System.Drawing.Size(77, 23)
+        Me.destino3x.TabIndex = 8
+        '
+        'destino2x
+        '
+        Me.destino2x.Enabled = False
+        Me.destino2x.Location = New System.Drawing.Point(81, 74)
+        Me.destino2x.Name = "destino2x"
+        Me.destino2x.Size = New System.Drawing.Size(77, 23)
+        Me.destino2x.TabIndex = 7
+        '
+        'destino1x
+        '
+        Me.destino1x.Enabled = False
+        Me.destino1x.Location = New System.Drawing.Point(81, 44)
+        Me.destino1x.Name = "destino1x"
+        Me.destino1x.Size = New System.Drawing.Size(77, 23)
+        Me.destino1x.TabIndex = 6
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(20, 210)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(43, 15)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Dest 6"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(20, 178)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(43, 15)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Dest 5"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(20, 145)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(43, 15)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Dest 4"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(20, 110)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(43, 15)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Dest 3"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(20, 76)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(43, 15)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Dest 2"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(20, 46)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(43, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Dest 1"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(81, 254)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(160, 23)
+        Me.Button1.TabIndex = 20
+        Me.Button1.Text = "Coordenada Aleatorea"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(895, 562)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.Text = "Reparto - Felipe Mancilla"
+        Me.Panel1.ResumeLayout(False)
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents destino1x As DomainUpDown
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents destino6x As DomainUpDown
+    Friend WithEvents destino5x As DomainUpDown
+    Friend WithEvents destino4x As DomainUpDown
+    Friend WithEvents destino3x As DomainUpDown
+    Friend WithEvents destino2x As DomainUpDown
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents destino6y As DomainUpDown
+    Friend WithEvents destino5y As DomainUpDown
+    Friend WithEvents destino4y As DomainUpDown
+    Friend WithEvents destino3y As DomainUpDown
+    Friend WithEvents destino2y As DomainUpDown
+    Friend WithEvents destino1y As DomainUpDown
+    Friend WithEvents Button1 As Button
 End Class
--- CS2PNETII/Form1.vb
+++ CS2PNETII/Form1.vb
@@ -1,3 +1,7 @@
 ﻿Public Class Form1
-
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim numAleatorio As New Random()
+        tbCoordX.Text = System.Convert.ToString(numAleatorio.Next(200, 1000))
+        tbCoordY.Text = System.Convert.ToString(numAleatorio.Next(200, 700))
+    End Sub
 End Class
